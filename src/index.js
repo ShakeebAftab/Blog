9f7d159a3f217c0dedb69739bd113853871b0787
ShakeebSheikh <shakeebaftab@gmail.com>
2021-07-09 16:29:52 +0500

    INIT

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Material-ui Imports
+import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+
+// Font Import
+import '@fontsource/roboto';
+
+// Theme Import
+import theme from './Theme/Theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
